485f7318e2a15e1e74518a5445ee616f5d332be7
imbukharihamid-pixel <imbukharihamid@gmail.com>
2026-03-12 23:09:13 +0500

    PF WEEK 3 work

---
+++ pF - WEEK 2/age.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int person_age,num_they_moved;
+cout<<"Enter the person's age : ";
+cin>>person_age;
+cout<<"Number of time they moved from one city to another : ";
+cin>>num_they_moved;
+int average_live_rate;
+average_live_rate=person_age/num_they_moved+1;
+cout<<"Enter the average years the person lived in a house : " <<average_live_rate ;
+
+}
---
+++ pF - WEEK 2/hamid1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+int ini_velocity , acceleration, time ,final_velocity;
+cout<<"ENTER THE INITIAL VELOCITY";
+cin>> ini_velocity;
+cout <<"ENTER THE ACCELERATION";
+cin>> acceleration;
+cout<<"ENTER THE TIME";
+cin>> time;
+acceleration = (final_velocity - ini_velocity)/time;
+cout<<"FINAL VELOCITY IS" <<final_velocity;
+
+
+}
